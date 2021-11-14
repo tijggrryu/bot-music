5c181ee66492c47dc43bef5e035e06e4cd80882f
zacNx14422 <89572858+zacNx14422@users.noreply.github.com>
2021-11-14 09:13:23 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,13 @@
 module.exports = async (client) => {
   (client.Ready = true),
     client.user.setPresence({
-      game: {
-        name: 'setup by zac',
-        type: "STREAMING",
-        url: "https://www.twitch.tv/"
-      },
-});
+        status: 'available',
+        activity: {
+            name: 'setup by zac',
+            type: 'STREAMING',
+            url: 'https://twitch.tv/'
+        }
+    });
   client.Manager.init(client.user.id);
   client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
   client.RegisterSlashCommands();
