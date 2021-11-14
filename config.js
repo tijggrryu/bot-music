a43661b387f467db1043dcd929b42b71a89904dd
zacNx14422 <89572858+zacNx14422@users.noreply.github.com>
2021-11-14 09:26:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,28 +6,28 @@ module.exports = {
   scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   serverDeafen: true, //If you want bot to stay deafened
   defaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
-  supportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
+  supportServer: "https://bit.ly/ZacDiscord", //Support Server Link
   permissions: 2205281600, //Bot Inviting Permissions
   nodes: [
     {
       identifier: "Main",
-      host: "",
-      port: 80,
-      password: "",
+      host: "lavalink-repl.sukprasert5144.repl.co",
+      port: 443,
+      password: "youshallnotpass",
       //retryAmount: 5, - Optional
       //retryDelay: 1000, - Optional
-      //secure: false - Optional | Default: false
+      secure: true //- Optional | Default: false
     },
   ], //Lavalink servers
   embedColor: "BLUE", //Color of the embeds
   presence: {
     //PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
-    status: "idle", // You can have online, idle, and dnd
+    status: "dnd", // You can have online, idle, and dnd
     activities: [
       {
-        name: "Music", //Status Text
-        type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
-        url: "https://twitch.tv/jeffbezos", // Link streaming url
+        name: "setup", //Status Text
+        type: "STREAMING", // PLAYING, WATCHING, LISTENING, STREAMING
+        url: "https://twitch.tv/", // Link streaming url
       },
     ],
   },
