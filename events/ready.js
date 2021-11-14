4466c08094915276e52ae045c1691d00cedc567a
zacNx14422 <89572858+zacNx14422@users.noreply.github.com>
2021-11-14 09:34:02 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,12 @@
 module.exports = async (client) => {
   (client.Ready = true),
     client.user.setPresence({
-        status: 'available',
-        activity: {
-            name: 'setup by zac',
-            type: 'STREAMING',
-            url: 'https://twitch.tv/'
-        }
-    });
+    status: client.botconfig.Presence.status, // You can show online, idle, and dnd
+      activity: {
+        name: client.botconfig.Presence.name,
+        type: client.botconfig.Presence.type,
+      },
+});
   client.Manager.init(client.user.id);
   client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
   client.RegisterSlashCommands();
